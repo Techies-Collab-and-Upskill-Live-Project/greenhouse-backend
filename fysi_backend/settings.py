99782672a8f536c42d118b4193c32404d72cf07a
Olawoore Damilola James <58667901+dammycute@users.noreply.github.com>
2024-10-03 19:54:37 +0100

    Update settings.py

--- fysi_backend/settings.py
+++ fysi_backend/settings.py
@@ -113,12 +113,12 @@ DATABASES = {
 
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-#EMAIL_PORT = os.getenv('EMAIL_PORT')
-#EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-#EMAIL_HOST = os.getenv('EMAIL_HOST')
-#EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
-#EMAIL_USE_TLS = True
-#EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+EMAIL_PORT = os.getenv('EMAIL_PORT')
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = os.getenv('EMAIL_HOST')
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_USE_TLS = True
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
